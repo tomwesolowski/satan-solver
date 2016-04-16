843df1714407f519ec186c8c23b176831b121359
mrfesol <tom.wesolowski92@gmail.com>
2016-04-16 16:34:56 +0200

    cmake initialization

---
+++ CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/src/solver.cpp.o"
+  "solver.pdb"
+  "solver"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/solver.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define ALL(v) v.begin(), v.end()
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPD(i, a, b) for (int i = a; i > b; i--)
+#define REPLL(i, a, b) for (ll i = a; i < b; i++)
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define FORD(i, a, b) for (int i = a; i >= b; i--)
+#define FORLL(i, a, b) for (ll i = a; i <= b; i++)
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef pair<ld, ld> pld;
+
+#define remax(a, b) a = max(a, b)
+#define remin(a, b) a = min(a, b)
+
+#define popcount __builtin_popcount
+#define pb push_back
+#define mp make_pair
+#define st first
+#define y first
+#define nd second
+#define x second
+
+#define eps 1e-9
+#define pi acos(-1.0)
+
+const int inf = 1e9 + 1;
+
+const int N = 100123;
+
+int z, n;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+
+  cin >> n;
+}
